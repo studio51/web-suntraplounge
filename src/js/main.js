4d094844e8f7d346f4c40ca2a2c1e3f20021c85a
Vlad Radulescu <pacMakaveli90@gmail.co.uk>
2016-09-14 17:08:56 +0100

    finish header and 'jumbotron' :sparkle:

--- src/js/main.js
+++ src/js/main.js
@@ -1,343 +1,333 @@
-'use strict';
+jQuery(document).ready(function() {
 
-/**
- * Demo.
- */
-var demo = (function(window, undefined) {
+;(function(window) {
 
-  /**
-   * Enum of CSS selectors.
-   */
-	var SELECTORS = {
-		pattern: '.pattern',
-		card: '.service',
-		cardImage: '.service-image',
-		cardClose: '.service-btn-close',
+	'use strict';
+
+	// taken from mo.js demos
+	function isIOSSafari() {
+		var userAgent;
+		userAgent = window.navigator.userAgent;
+		return userAgent.match(/iPad/i) || userAgent.match(/iPhone/i);
 	};
 
-	/**
-	 * Enum of CSS classes.
-	 */
-	var CLASSES = {
-		patternHidden: 'pattern--hidden',
-		polygon: 'polygon',
-		polygonHidden: 'polygon--hidden'
+	// taken from mo.js demos
+	function isTouch() {
+		var isIETouch;
+		isIETouch = navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+		return [].indexOf.call(window, 'ontouchstart') >= 0 || isIETouch;
 	};
 
+	// taken from mo.js demos
+	var isIOS = isIOSSafari(),
+		clickHandler = isIOS || isTouch() ? 'touchstart' : 'click';
 
-  /**
-   * Map of svg paths and points.
-   */
-  var polygonMap = {
-    paths: null,
-    points: null
-  };
+	function extend( a, b ) {
+		for( var key in b ) {
+			if( b.hasOwnProperty( key ) ) {
+				a[key] = b[key];
+			}
+		}
+		return a;
+	}
 
-  /**
-   * Container of Card instances.
-   */
-  var layout = {};
+	function Animocon(el, options) {
+		this.el = el;
+		this.options = extend( {}, this.options );
+		extend( this.options, options );
 
-  /**
-   * Initialise demo.
-   */
-  function init() {
+		this.checked = false;
 
-    // For options see: https://github.com/qrohlf/Trianglify
-    var pattern = Trianglify({
-      width: window.innerWidth,
-      height: window.innerHeight,
-      cell_size: 60,
-      variance: 1,
-      stroke_width: 1,
-      x_colors: ['#221D52', '#CDE7EA', '#221D52']
-    }).svg(); // Render as SVG.
+		this.timeline = new mojs.Timeline();
 
-    _mapPolygons(pattern);
+		for(var i = 0, len = this.options.tweens.length; i < len; ++i) {
+			this.timeline.add(this.options.tweens[i]);
+		}
 
-    _bindCards();
-  };
+		var self = this;
+		this.el.addEventListener(clickHandler, function() {
+			if( self.checked ) {
+				self.options.onUnCheck();
+			}
+			else {
+				self.options.onCheck();
+				self.timeline.replay();
+			}
+			self.checked = !self.checked;
+		});
+	}
 
-  /**
-   * Store path elements, map coordinates and sizes.
-   * @param {Element} pattern The SVG Element generated with Trianglify.
-   * @private
-   */
-  function _mapPolygons(pattern) {
+	Animocon.prototype.options = {
+		tweens : [
+			new mojs.Burst({})
+		],
+		onCheck : function() { return false; },
+		onUnCheck : function() { return false; }
+	};
 
-    // Append SVG to pattern container.
-    $(SELECTORS.pattern).append(pattern);
+	function init() {
 
-    // Convert nodelist to array,
-    // Used `.childNodes` because IE doesn't support `.children` on SVG.
-    polygonMap.paths = [].slice.call(pattern.childNodes);
+    var el7 = $('.navbar-trigger'), el7span = $('.navbar-trigger-icon');
+    new Animocon(el7[0], {
+      tweens : [
+        
+        new mojs.Burst({
+          parent: el7[0],
+          radius: { 90:150 },
+          count:  18,
 
-    polygonMap.points = [];
+          children: {
+            fill:     '#774D37',
+            opacity:   0.6,
+            scale:     1,
+            radius:    { 'rand(5,20)':0 },
+            swirlSize: 15,
+            direction: [ 1, 1, -1, -1, 1, 1, -1, -1, -1 ],
+            duration:  1200,
+            delay:     200,
+            easing:    mojs.easing.bezier(0.1, 1, 0.3, 1),
+            isSwirl:   true
+          }
+        }),
 
-    polygonMap.paths.forEach(function(polygon) {
+        // ring animation
+        new mojs.Shape({
+          parent:      el7[0],
+          radius:      { 30: 100 },
+          fill:        'transparent',
+          stroke:      '#774D37',
+          strokeWidth: { 30:0 },
+          opacity:     0.6,
+          duration:    1500,
+          easing:      mojs.easing.bezier(0.1, 1, 0.3, 1)
+        }),
 
-      // Hide polygons by adding CSS classes to each svg path (used attrs because of IE).
-      $(polygon).attr('class', CLASSES.polygon);
+        new mojs.Shape({
+          parent:   el7[0],
+          radius:   {30: 80},
+          fill:   'transparent',
+          stroke:   '#774D37',
+          strokeWidth: { 20:0 },
+          opacity:  0.3,
+          duration:   1600,
+          delay:  320,
+          easing:   mojs.easing.bezier(0.1, 1, 0.3, 1)
+        }),
+        // icon scale animation
+        new mojs.Tween({
+          duration : 1000,
+          onUpdate: function(progress) {
+            if(progress > 0.3) {
+              var elasticOutProgress = mojs.easing.elastic.out(1.43*progress-0.43);
+              el7span[0].style.WebkitTransform = el7span[0].style.transform = 'scale3d(' + elasticOutProgress + ',' + elasticOutProgress + ',1)';
+            }
+            else {
+              el7span[0].style.WebkitTransform = el7span[0].style.transform = 'scale3d(0,0,1)';
+            }
+          }
+        })
+      ],
 
-      var rect = polygon.getBoundingClientRect();
+      onCheck: function() {
+        el7[0].style.color = '#AACFBE';
 
-      var point = {
-        x: rect.left + rect.width / 2,
-        y: rect.top + rect.height / 2
-      };
+        el7.addClass('opened');
+        el7span.addClass('opened');
+        $('#navbar-menu').addClass('opened');
+      },
 
-      polygonMap.points.push(point);
-    });
+      onUnCheck: function() {
+        el7[0].style.color = '#C0C1C3';
 
-    // All polygons are hidden now, display the pattern container.
-    $(SELECTORS.pattern).removeClass(CLASSES.patternHidden);
-  };
-
-  /**
-   * Bind Card elements.
-   * @private
-   */
-  function _bindCards() {
-
-    var elements = $(SELECTORS.card);
-
-    $.each(elements, function(card, i) {
-
-      var instance = new Card(i, card);
-
-      layout[i] = {
-        card: instance
-      };
-
-      var cardImage = $(card).find(SELECTORS.cardImage);
-      var cardClose = $(card).find(SELECTORS.cardClose);
-
-      $(cardImage).on('click', _playSequence.bind(this, true, i));
-      $(cardClose).on('click', _playSequence.bind(this, false, i));
-    });
-  };
-
-  /**
-   * Create a sequence for the open or close animation and play.
-   * @param {boolean} isOpenClick Flag to detect when it's a click to open.
-   * @param {number} id The id of the clicked card.
-   * @param {Event} e The event object.
-   * @private
-   *
-   */
-  function _playSequence(isOpenClick, id, e) {
-
-    var card = layout[id].card;
-
-    // Prevent when card already open and user click on image.
-    if (card.isOpen && isOpenClick) return;
-
-    // Create timeline for the whole sequence.
-    var sequence = new TimelineLite({paused: true});
-
-    var tweenOtherCards = _showHideOtherCards(id);
-
-    if (!card.isOpen) {
-      // Open sequence.
-
-      _setPatternBgImg(e.target);
-
-      sequence.add(tweenOtherCards);
-      sequence.add(card.openCard(_onCardMove), 0);
-
-    } else {
-      // Close sequence.
-
-      var closeCard = card.closeCard();
-      var position = closeCard.duration() * 0.8; // 80% of close card tween.
-
-      sequence.add(closeCard);
-      sequence.add(tweenOtherCards, position);
-    }
-
-    sequence.play();
-  };
-
-  /**
-   * Show/Hide all other cards.
-   * @param {number} id The id of the clcked card to be avoided.
-   * @private
-   */
-  function _showHideOtherCards(id) {
-
-    var TL = new TimelineLite;
-
-    var selectedCard = layout[id].card;
-
-    for (var i in layout) {
-
-      var card = layout[i].card;
-
-      // When called with `openCard`.
-      if (card.id !== id && !selectedCard.isOpen) {
-        TL.add(card.hideCard(), 0);
-      }
-
-      // When called with `closeCard`.
-      if (card.id !== id && selectedCard.isOpen) {
-        TL.add(card.showCard(), 0);
-      }
-    }
-
-    return TL;
-  };
-
-  /**
-   * Add card image to pattern background.
-   * @param {Element} image The clicked SVG Image Element.
-   * @private
-   */
-  function _setPatternBgImg(image) {
-
-    var imagePath = $(image).attr('xlink:href');
-
-    $(SELECTORS.pattern).css('background-image', 'url(' + imagePath + ')');
-  };
-
-  /**
-   * Callback to be executed on Tween update, whatever a polygon
-   * falls into a circular area defined by the card width the path's
-   * CSS class will change accordingly.
-   * @param {Object} track The card sizes and position during the floating.
-   * @private
-   */
-  function _onCardMove(track) {
-
-    var radius = track.width / 2;
-
-    var center = {
-      x: track.x,
-      y: track.y
-    };
-
-    polygonMap.points.forEach(function(point, i) {
-
-      if (_detectPointInCircle(point, radius, center)) {
-        $(polygonMap.paths[i]).attr('class', CLASSES.polygon + ' ' + CLASSES.polygonHidden);
-      } else {
-        $(polygonMap.paths[i]).attr('class', CLASSES.polygon);
+        el7.removeClass('opened');
+        el7span.removeClass('opened');
+        $('#navbar-menu').removeClass('opened');
       }
     });
-  }
+    /* Icon 7 */
 
-  /**
-   * Detect if a point is inside a circle area.
-   * @private
-   */
-  function _detectPointInCircle(point, radius, center) {
+    /* Icon 8 */
+    // var el8 = items[7].querySelector('button.icobutton'), el8span = el8.querySelector('span');
+    // var scaleCurve8 = mojs.easing.path('M0,100 L25,99.9999983 C26.2328835,75.0708847 19.7847843,0 100,0');
+    // new Animocon(el8, {
+    //  tweens : [
+    //  // burst animation
+    //  new mojs.Burst({
+    //  parent:   el8,
+		// 			count: 				28,
+		// 			radius: 			{50:110},
+		// 			children: {
+		// 				fill: 			'#988ADE',
+		// 				opacity: 		0.6,
+		// 				radius: 		{'rand(5,20)':0},
+		// 				scale: 			1,
+		// 				swirlSize: 	15,
+		// 				duration: 	1600,
+		// 				easing: 		mojs.easing.bezier(0.1, 1, 0.3, 1),
+		// 				isSwirl: 		true
+		// 			}
+		// 		}),
+		// 		// burst animation
+		// 		new mojs.Burst({
+		// 			parent: 	el8,
+		// 			count: 		18,
+		// 			angle: 		{0:10},
+		// 			radius: 	{140:200},
+		// 			children: {
+		// 				fill: 			'#988ADE',
+		// 				shape: 			'line',
+		// 				opacity: 		0.6,
+		// 				radius: 		{'rand(5,20)':0},
+		// 				scale: 			1,
+		// 				stroke: 		'#988ADE',
+		// 				strokeWidth: 2,
+		// 				duration: 	1800,
+		// 				delay: 			300,
+		// 				easing: 		mojs.easing.bezier(0.1, 1, 0.3, 1)
+		// 			}
+		// 		}),
+		// 		// burst animation
+		// 		new mojs.Burst({
+		// 			parent: 	el8,
+		// 			radius: 	{40:80},
+		// 			count: 		18,
+		// 			children: {
+		// 				fill: 			'#988ADE',
+		// 				opacity: 		0.6,
+		// 				radius: 		{'rand(5,20)':0},
+		// 				scale: 			1,
+		// 				swirlSize:  15,
+		// 				duration: 	2000,
+		// 				delay: 			500,
+		// 				easing: 		mojs.easing.bezier(0.1, 1, 0.3, 1),
+		// 				isSwirl: 		true
+		// 			}
+		// 		}),
+		// 		// burst animation
+		// 		new mojs.Burst({
+		// 			parent: 	el8,
+		// 			count: 		20,
+		// 			angle: 		{0:-10},
+		// 			radius: 	{90:130},
+		// 			children: {
+		// 				fill: 			'#988ADE',
+		// 				opacity: 		0.6,
+		// 				radius: 		{'rand(10,20)':0},
+		// 				scale: 			1,
+		// 				duration: 	3000,
+		// 				delay: 			750,
+		// 				easing: 		mojs.easing.bezier(0.1, 1, 0.3, 1)
+		// 			}
+		// 		}),
+		// 		// icon scale animation
+		// 		new mojs.Tween({
+		// 			duration : 400,
+		// 			easing: mojs.easing.back.out,
+		// 			onUpdate: function(progress) {
+		// 				var scaleProgress = scaleCurve8(progress);
+		// 				el8span.style.WebkitTransform = el8span.style.transform = 'scale3d(' + progress + ',' + progress + ',1)';
+		// 			}
+		// 		})
+		// 	],
+		// 	onCheck : function() {
+		// 		el8.style.color = '#988ADE';
+		// 	},
+		// 	onUnCheck : function() {
+		// 		el8.style.color = '#C0C1C3';
+		// 	}
+		// });
+		// /* Icon 8 */
 
-    var xp = point.x;
-    var yp = point.y;
 
-    var xc = center.x;
-    var yc = center.y;
+	}
 
-    var d = radius * radius;
-
-    var isInside = Math.pow(xp - xc, 2) + Math.pow(yp - yc, 2) <= d;
-
-    return isInside;
-  };
-
-  // Expose methods.
-  return {
-    init: init
-  };
+	init();
 
 })(window);
 
-// Kickstart Demo.
-window.onload = demo.init;
+// function getUserLocation() {
+// 	var input = jQuery('input[name="saddr"]');
+//
+// 	function getLocation() {
+// 		if (navigator.geolocation) {
+// 			navigator.geolocation.getCurrentPosition(getPosition);
+// 		} else {
+// 			input.val('Gelocation is not supported by your browser, please input your Post Code manually..');
+// 		}
+// 	}
+//
+// 	function getPosition(position) {
+// 		input.val(position.coords.latitude + ', ' + position.coords.longitude)
+// 	}
+// }
 
-function getUserLocation() {
-	var input = jQuery('input[name="saddr"]');
+  // jQuery('.lazy').unveil();
 
-	function getLocation() {
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(getPosition);
-		} else {
-			input.val('Gelocation is not supported by your browser, please input your Post Code manually..');
-		}
-	}
+  // $('.btn-action').on('click', function(event) {
+	// 	event.preventDefault();
+  //
+  //   var $this  = $(this),
+  //       target = $this.data('target'),
+  //       klass  = 'opened';
+  //
+	// 	if (!open) {
+	// 		$(this).addClass(klass);
+	// 		$(target).addClass(klass);
+  //
+	// 		open = true;
+	// 	} else {
+	// 		$(this).removeClass(klass);
+	// 		$(target).removeClass(klass);
+  //
+	// 		open = false;
+	// 	}
+	// });
 
-	function getPosition(position) {
-		input.val(position.coords.latitude + ', ' + position.coords.longitude)
-	}
-}
-
-jQuery(document).ready(function() {
-  jQuery('.lazy').unveil();
-
-	var map = new GMaps({
-		el: '#map',
-		lat: 53.048532,
-		lng: -2.272429
-	});
-
-	map.addMarker({
-		lat: 53.048532,
-		lng: -2.262429,
-		title: 'Lima'
-	});
-
-	var open = false;
-
-	jQuery(window).on('scroll touchmove', function() {
-		jQuery('.navbar').toggleClass('navbar-small', jQuery(document).scrollTop() > 0);
-	});
-
-	jQuery('.btn-action').on('click', function(event) {
-		event.preventDefault();
-
-		var target = jQuery(this).data('target');
-
-		if (!open) {
-			jQuery(this).addClass('opened');
-			jQuery(target).addClass('opened');
-
-			open = true;
-		} else {
-			jQuery(this).removeClass('opened');
-			jQuery(target).removeClass('opened');
-
-			open = false;
-		}
-	});
-
-	jQuery('.tabs li').on('click', function(event) {
-		event.preventDefault();
-
-		if(!jQuery(this).hasClass('active')) {
-			var tab = jQuery(this).index();
-			var child = tab + 1;
-
-			jQuery('.tabs li.active').removeClass('active');
-			jQuery(this).addClass('active');
-			jQuery('.tabs-container .tab-pane.active').removeClass('active');
-			jQuery('.tabs-container .tab-pane:nth-child(' + child + ')').addClass('active');
-		};
-	});
+	// var map = new GMaps({
+	// 	el: '#map',
+	// 	lat: 53.048532,
+	// 	lng: -2.272429
+	// });
+	//
+	// map.addMarker({
+	// 	lat: 53.048532,
+	// 	lng: -2.262429,
+	// 	title: 'Lima'
+	// });
+	//
+	// var open = false;
 
 
-	var random = Math.floor(Math.random() * jQuery('#testimonials li').length);
-	jQuery('#testimonials li').hide().eq(random).show();
 
-	// Function to cycle through the reviews by fading them in/out
+	// jQuery('.tabs li').on('click', function(event) {
+	// 	event.preventDefault();
+	//
+	// 	if(!jQuery(this).hasClass('active')) {
+	// 		var tab = jQuery(this).index();
+	// 		var child = tab + 1;
+	//
+	// 		jQuery('.tabs li.active').removeClass('active');
+	// 		jQuery(this).addClass('active');
+	// 		jQuery('.tabs-container .tab-pane.active').removeClass('active');
+	// 		jQuery('.tabs-container .tab-pane:nth-child(' + child + ')').addClass('active');
+	// 	};
+	// });
 
-	(function showNextTestimonial() {
-		jQuery('#testimonials li:visible').delay(7500).fadeOut('slow', function() {
-			jQuery(this).appendTo('#testimonials ul');
 
-			jQuery('#testimonials li:first').fadeIn('slow', function() {
-				showNextTestimonial();
-			});
-		});
-	})();
-
-	getUserLocation();
+	// var random = Math.floor(Math.random() * jQuery('#testimonials li').length);
+	// jQuery('#testimonials li').hide().eq(random).show();
+	//
+	// // Function to cycle through the reviews by fading them in/out
+	//
+	// (function showNextTestimonial() {
+	// 	jQuery('#testimonials li:visible').delay(7500).fadeOut('slow', function() {
+	// 		jQuery(this).appendTo('#testimonials ul');
+	//
+	// 		jQuery('#testimonials li:first').fadeIn('slow', function() {
+	// 			showNextTestimonial();
+	// 		});
+	// 	});
+	// })();
+	//
+	// getUserLocation();
 });
